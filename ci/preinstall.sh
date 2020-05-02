dd5ccd82bf7b8ee5237855eb2540bb1a2a8b8e70
Hassan Khousheish <khousheish@live.com>
2020-05-02 13:48:09 +0300

    Change To Bash

--- ci/preinstall.sh
+++ ci/preinstall.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 
 cp src/environments/environment.test.ts.dist src/environments/environment.ts
 cp src/environments/environment.test.ts.dist src/environments/environment.prod.ts
