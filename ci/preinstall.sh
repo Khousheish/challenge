190637a30f40e342b1b913b8f5f9980f2ab501d7
Hassan Khousheish <khousheish@live.com>
2020-05-02 14:05:17 +0300

    Fix Preinstall Script

--- ci/preinstall.sh
+++ ci/preinstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp src/environments/environment.test.ts.dist src/environments/environment.ts
-cp src/environments/environment.test.ts.dist src/environments/environment.prod.ts
+cp src/environments/environment.test.ts src/environments/environment.ts
+cp src/environments/environment.test.ts src/environments/environment.prod.ts
