f9182de36c86edc6383f7300fc5af1160d561e35
Hassan Khousheish <khousheish@live.com>
2020-05-02 14:34:44 +0300

    Fix Karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,77 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: [
+      'jasmine',
+      '@angular-devkit/build-angular',
+    ],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-mocha-reporter'),
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    client:{
+      clearContext: false,
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/challenge'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: [
+        'html',
+        'lcovonly',
+      ],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    files: [
+      'src/app/spec/mocks/js/google-mock.js',
+      {
+        pattern: 'src/assets/**',
+        watched: false,
+        included: false,
+        nocache: false,
+        served: true,
+      },
+    ],
+    proxies: {
+      '/spa/assets/': '/base/src/assets/',
+    },
+    reporters: [
+      'mocha',
+      'progress',
+      'kjhtml',
+    ],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+      ChromiumHeadlessNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox'],
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    mochaReporter: {
+      ignoreSkipped: true,
+      colors: {
+        success: 'green',
+        info: 'blue',
+        warning: 'yellow / orange',
+        error: 'red',
+      },
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      },
+    },
   });
 };
